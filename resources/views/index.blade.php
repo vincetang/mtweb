7947ae126543d70812b37d47ab4acd9fe7915f33
Vincent Tang <Vince@Vincents-MacBook-Pro.local>
2016-02-06 02:44:21 -0500

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,333 @@
+<!-- Designed and created by Vincent Tang using bootstrap and implemented on Laravel 5 
+     JUly 2015 -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Melissa Twist</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <link href='http://fonts.googleapis.com/css?family=Monsieur+La+Doulaise' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Rouge+Script' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/stylesheets/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+   <!--  <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+ -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">MelissaTwist.ca</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#events">Events</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#gallery">Concierge</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#gallery">Makeup</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#gallery">Retreats</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <div class="logo-container">
+                    <img src="img/logo_wht.png" class="logo" />
+                </div>
+                <h1>Melissa Twist</h1>
+                <h2><span class="cursive-text main-page-text">Servicing Southern Georgian Bay</span></h2>
+                <hr>
+                <p><span class="main-page-sub-text">More to come... Stay tuned!</span></p>
+                <!-- <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a> -->
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading cursive-header">About Melissa</h2>
+                    <hr class="light">
+                    <p class="text-faded cursive-text about-text">With over 10 years of events industry experience, a degree in Tourism and Environmental Studies, a diploma in Special Events, and a certificate in Wedding Coordination, Melissa will be able to manage any type of event that you would like to host. As Event Coordinator at various high-end Oakville & Toronto venues, Melissa has worked over five hundred weddings and an equal amount of private and corporate functions throughout her career. While living in Australia, Melissa assisted an events team in planning, organizing and operating a national 5-day music and art festival with 500,000 attendees that generated over $1 million in revenue.  Her academic and job related experience has enhanced her ability to be creative, adding a unique flare to each event she organizes.</p>
+                    <br>
+                    <!-- Linkedin button and link -->
+                   <!--  <a target="_blank" href="https://ca.linkedin.com/pub/melissa-twist/27/901/8a2" style="text-decoration:none;"><span style="font: 80% Arial,sans-serif; color:white;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Melissa Twist's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View Melissa Twist's profile</span></a> -->
+                    <!-- <a href="#" class="btn btn-default btn-xl">Get Started!</a> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- EVENTS SECTION with font-awesome icons -->
+    <section id="events">
+        <div class="container events-container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading-dark cursive-header">Events</h2>
+                    <hr class="primary events-bar">
+                </div>
+            </div>
+        </div>
+        <div class="container events-icon-container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img class="events-icon" src="img/icons/star.png">
+                        <!-- <i class="fa fa-4x fa-star wow bounceIn text-primary fa-color"></i> -->
+                        <h3 class="text-muted cursive-text events-icon-text">Venues</h3>
+                        <!-- <p class="text-muted">Venues</p> -->
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img class="events-icon" src="img/icons/cuisine.png">
+                        <!-- <i class="fa fa-4x fa-cutlery wow bounceIn text-primary fa-color" data-wow-delay=".1s"></i> -->
+                        <h3 class="text-muted cursive-text events-icon-text">Cuisine</h3>
+                        <!-- <p class="text-muted">Cuisine</p> -->
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img class="events-icon" src="img/icons/service.png">
+                        <!-- <i class="fa fa-4x fa-users wow bounceIn text-primary fa-color" data-wow-delay=".2s"></i> -->
+                        <h3 class="text-muted cursive-text events-icon-text">Service</h3>
+                        <!-- <p class="text-muted">Service</p> -->
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img class="events-icon" src="img/icons/diamond.png">
+                        <!-- <i class="fa fa-4x fa-diamond wow bounceIn text-primary fa-color" data-wow-delay=".3s"></i> -->
+                        <h3 class="text-muted cursive-text events-icon-text">Luxury</h3>
+                        <!-- <p class="text-muted">Luxury</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<!-- GALLERY SECTION -->
+    <section class="no-padding" id="gallery">
+        <div class="container-fluid" id="gallery-container">
+            <div class="row row-center">
+
+                <!-- Events image -->
+                <div class="col-lg-6 col-sm-12 text-center">
+                    <a class="portfolio-box gapless">
+                        <img src="img/portfolio/ladder.jpg" class="img-responsive img-fill" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <!--
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                -->
+                                <div class="project-name">
+                                    <h3 class="cursive-text gallery-text">Events</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- Makeup image -->
+                <div class="col-lg-6  col-sm-12 text-center">
+                    <a class="portfolio-box gapless">
+                        <img src="img/portfolio/makeup.png" class="img-responsive img-fill" alt="">
+                        <div class="portfolio-box-caption  id="instagram-text"">
+                            <div class="portfolio-box-caption-content">
+                                <!--
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                -->
+                                <div class="project-name">
+                                    <h3 class="cursive-text gallery-text">Makeup</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                
+                <div class="col-lg-6 col-sm-12">
+                    <a class="portfolio-box gapless">
+                        <img src="img/portfolio/concierge.png" class="img-responsive img-fill" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <!--
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                -->
+                                <div class="project-name">
+                                    <h3 class="cursive-text gallery-text">Concierge</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-6 col-sm-12">
+                    <a class="portfolio-box gapless">
+                        <img src="img/portfolio/retreat.jpg" class="img-responsive img-fill" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <!--
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                -->
+                                <div class="project-name">
+                                    <h3 class="cursive-text gallery-text">Retreats</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+<!-- CONTACT SECTION -->
+    <section id="contact">
+
+        <!-- contact section text -->
+        <div class="container contacts-container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading contact-text cursive-header">Find out more about our services</h2>
+                    <hr class="primary contact-bar">
+                    <p class="contact-text cursive-text">Ready to plan your next event with Melissa? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+            </div>
+
+            <!-- row containing email and phone number -->
+            <div class="row">
+                 <div class="col-lg-4 text-center">
+                    <!-- <i class="fa fa-phone fa-3x wow bounceIn contact-icon"></i> -->
+                    <img class="contacts-icon" src="img/icons/phone2.png">
+                    <a href="tel:+1-416-997-4141"><p class="phonenumber">(416) 997-4141</p></a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <!-- <i class="fa fa-phone fa-3x wow bounceIn contact-icon"></i> -->
+                    <img class="contacts-icon" src="img/icons/phone1.png">
+                    <a href="tel:+1-705-445-0774"><p class="phonenumber">(705) 445-0774</p></a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <!-- <i class="fa fa-envelope-o fa-3x wow bounceIn contact-icon" data-wow-delay=".1s"></i> -->
+                    <img class="contacts-icon" src="img/icons/email.png">
+                    <a href="mailto:info@melissatwist.ca"><p id="email-text">info@melissatwist.ca</p></a>
+                </div>
+
+                <!-- "//badges.instagram.com/static/images/ig-badge-48.png" -->
+      <!--           <div class="col-lg-4 text-center">
+                    <a target="_blank" class="fa fa-3x wow bounceIn contact-icon" href="http://instagram.com/melissatwist1?ref=badge" class="ig-b- ig-b-48"><img src="img/instagram.png"  alt="Instagram" /></a>
+                    <a target="_blank"  href="http://instagram.com/melissatwist1?ref=badge"><p id="instagram-text">melissatwist1</p></a>
+                </div> -->
+            </div>
+
+            <!-- social media buttons -->
+            <div class='col-lg-12 social-media'>
+                <a>
+                    <i class='icon-facebook'></i>
+                </a>
+                <a>
+                 <i class='icon-twitter'></i>
+                </a>
+
+                <a target="_blank" href="https://ca.linkedin.com/pub/melissa-twist/27/901/8a2">
+                    <i class='icon-linkedin'></i>
+                </a>
+
+                <a>
+                    <i class='icon-pinterest'></i>
+                </a>
+
+                <a target="_blank" href="http://instagram.com/melissatwist1?ref=badge">
+                    <i class='icon-instagram'></i>
+                </a>
+                </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+</body>
+
+</html>
